0c23fc4e7a444887a3e83bf706d88ca6f37b9db5
Ken Hall <kenhallthe3rd@gmail.com>
2018-06-14 19:57:50 -0700

    initial commit

---
+++ Assignment_4_Files/Assignment_4_Files/cmake-build-debug/CMakeFiles/Assignment_4_Files.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment_4_Files.dir/bst.c.obj"
+  "CMakeFiles/Assignment_4_Files.dir/compare.c.obj"
+  "Assignment_4_Files.pdb"
+  "Assignment_4_Files.exe"
+  "Assignment_4_Files.exe.manifest"
+  "libAssignment_4_Files.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment_4_Files.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
